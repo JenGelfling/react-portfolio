7854233d5efa6f1998a07cae8de6878ea8e9c9de
Ben Merchant <benmerch@gmail.com>
2024-10-06 21:29:44 -0500

    Added a couple projects to the portfolio.

--- my-portfolio/src/components/AboutMe.jsx
+++ my-portfolio/src/components/AboutMe.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const AboutMe = () => (
   <section>
     <img src="/assets/headshot.jpeg" alt="Ben Merchant's pic" />
-    <p>Short bio about yourself...</p>
+    <p>
+      I just completed the U of M coding bootcamp to learn to be a full stack
+      developer. I have worked for software companies doing technical support,
+      client training, and project coordination. I am also a trained track &
+      field coach, massage therapist, and yoga instructor. I enjoy helping
+      others achieve their goals.
+    </p>
   </section>
 );
 
--- my-portfolio/src/components/Portfolio.jsx
+++ my-portfolio/src/components/Portfolio.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 const projects = [
   {
-    title: "Project 1",
-    imageUrl: "project1-image-url",
-    deployedUrl: "deployed-url-1",
+    title: "Horiseon Homepage",
+    imageUrl: "../assets/horiseon-homepage.png",
+    deployedUrl: "https://jengelfling.github.io/accessibility-seo-cleanup/",
     githubUrl: "github-url-1",
+    description:
+      "In the Horiseon SEO and accessibility project we refactored the HTML and CSS to follow best practices around semantic elements and reducing redundancies.",
   },
-  // Add more projects...
+  {
+    title: "Moonshadow Bodywork",
+    imageUrl: "../assets/moonshadow-bodywork.png",
+    deployedUrl: "https://www.moonshadowbodywork.com/",
+    githubUrl: "github-url-2",
+    description: "This is the homepage for my Massage Therapy business.",
+  },
+  // Add more projects as needed...
 ];
 
 const Portfolio = () => (
@@ -16,10 +25,32 @@ const Portfolio = () => (
     <div>
       {projects.map((project) => (
         <div key={project.title}>
-          <img src={project.imageUrl} alt={project.title} />
-          <h3>{project.title}</h3>
-          <a href={project.deployedUrl}>View Live</a>
-          <a href={project.githubUrl}>View Code</a>
+          <a
+            href={project.deployedUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="space"
+          >
+            <img
+              src={project.imageUrl}
+              alt={project.title}
+              width="400"
+              height="250"
+            />
+          </a>
+          <h3>
+            <a
+              href={project.deployedUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {project.title}
+            </a>
+          </h3>
+          <p>{project.description}</p>
+          <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
+            View Code
+          </a>
         </div>
       ))}
     </div>
