ef8056c999395272d98f271c7210a5772fab94c6
Ben Merchant <benmerch@gmail.com>
2024-10-18 10:11:49 -0500

    Added some styling and debugging for sections.

--- my-portfolio/src/components/AboutMe.jsx
+++ my-portfolio/src/components/AboutMe.jsx
@@ -3,22 +3,24 @@ import "../App.css";
 
 const AboutMe = () => (
   <div className="section">
-    <p>
-      I recently completed the University of Minnesota coding bootcamp, where I
-      trained to become a full stack developer using the MERN stack — MongoDB,
-      Express.js, React, and Node.js. While I've previously worked with HTML,
-      CSS, and some SQL, this bootcamp allowed me to build applications from
-      front end to back end for the first time.
-    </p>
-    <p>
-      My professional background includes experience in technical support,
-      client training, and project coordination within software companies. I
-      also have the rewarding experience of tutoring a student in C# to develop
-      a game in Unity, which further deepens my understanding of programming and
-      game design. Additionally, I am or have been, a certified track & field
-      coach, massage therapist, and yoga instructor, all of which reflect my
-      passion for helping others achieve their goals.
-    </p>
+    <div>
+      <p>
+        I recently completed the University of Minnesota coding bootcamp, where
+        I trained to become a full stack developer using the MERN stack —
+        MongoDB, Express.js, React, and Node.js. While I've previously worked
+        with HTML, CSS, and some SQL, this bootcamp allowed me to build
+        applications from front end to back end for the first time.
+      </p>
+      <p>
+        My professional background includes experience in technical support,
+        client training, and project coordination within software companies. I
+        also have the rewarding experience of tutoring a student in C# to
+        develop a game in Unity, which further deepens my understanding of
+        programming and game design. Additionally, I am or have been, a
+        certified track & field coach, massage therapist, and yoga instructor,
+        all of which reflect my passion for helping others achieve their goals.
+      </p>
+    </div>
   </div>
 );
 
